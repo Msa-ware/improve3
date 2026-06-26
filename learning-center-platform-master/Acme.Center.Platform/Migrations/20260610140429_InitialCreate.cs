9feb07bf07489ced1ae42069fed512c0cbea5a7f
Msa_ware <73900517+Msa-ware@users.noreply.github.com>
2026-06-26 17:38:38 -0500

    Add files via upload

---
+++ learning-center-platform-master/Acme.Center.Platform/Migrations/20260610140429_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Acme.Center.Platform.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "profiles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    first_name = table.Column<string>(type: "longtext", nullable: false),
+                    last_name = table.Column<string>(type: "longtext", nullable: false),
+                    email_address = table.Column<string>(type: "longtext", nullable: false),
+                    address_street = table.Column<string>(type: "longtext", nullable: false),
+                    address_number = table.Column<string>(type: "longtext", nullable: false),
+                    address_city = table.Column<string>(type: "longtext", nullable: false),
+                    address_postal_code = table.Column<string>(type: "longtext", nullable: false),
+                    address_country = table.Column<string>(type: "longtext", nullable: false),
+                    created_at = table.Column<DateTimeOffset>(type: "datetime", nullable: true),
+                    updated_at = table.Column<DateTimeOffset>(type: "datetime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("p_k_profiles", x => x.id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "profiles");
+        }
+    }
+}
